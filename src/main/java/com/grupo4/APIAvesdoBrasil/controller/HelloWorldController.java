598772fdf764958506637fca3da02ed07009444d
beatrizfteixeira <biaft03@protonmail.com>
2023-07-10 16:17:22 -0300

    first commit

---
+++ src/main/java/com/grupo4/APIAvesdoBrasil/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.grupo4.APIAvesdoBrasil.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloWorldController {
+
+
+    // http://localhost:8080/helloworld
+    @GetMapping("helloworld")
+    public String sayHelloWorld() {
+        return "hellooo";
+    }
+
+}
