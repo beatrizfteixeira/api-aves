ec09052c1b35baf6596adc25f3e14dcfcf48a461
Ricardo Matsumura <fuzilo@hotmail.com>
2023-07-10 16:03:53 -0300

    Commit Inicial, projeto criado

---
+++ src/main/java/com/grupo4/APIAvesdoBrasil/ApiAvesdoBrasilApplication.java
@@ -0,0 +1,13 @@
+package com.grupo4.APIAvesdoBrasil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAvesdoBrasilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAvesdoBrasilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo4/APIAvesdoBrasil/ApiAvesdoBrasilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo4.APIAvesdoBrasil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAvesdoBrasilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
