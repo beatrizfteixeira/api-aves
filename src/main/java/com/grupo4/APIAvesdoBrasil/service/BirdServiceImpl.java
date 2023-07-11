07ebe80c32707dba567d1d5c2cbc3d5187c8af82
Ricardo Matsumura <fuzilo@hotmail.com>
2023-07-11 17:54:18 -0300

    Made Annotations in entity model, implemented requisition methods as springDataJpa form

--- src/main/java/com/grupo4/APIAvesdoBrasil/service/BirdServiceImpl.java
+++ src/main/java/com/grupo4/APIAvesdoBrasil/service/BirdServiceImpl.java
@@ -1,11 +1,21 @@
 package com.grupo4.APIAvesdoBrasil.service;
 
 import com.grupo4.APIAvesdoBrasil.entity.Bird;
+import com.grupo4.APIAvesdoBrasil.repository.BirdsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class BirdServiceImpl implements BirdService{
 
+    private BirdsRepository birdsRepository;
+    @Autowired
+    public BirdServiceImpl(BirdsRepository theBirdsRepository){
+        birdsRepository = theBirdsRepository;
+    }
     @Override
     public List<Bird> findAll() {
         return null;
@@ -13,16 +23,26 @@ public class BirdServiceImpl implements BirdService{
 
     @Override
     public Bird findById(int id) {
-        return null;
+        Optional<Bird> result = birdsRepository.findById(id);
+        Bird bird = null;
+
+        if (result.isPresent()){
+            bird = result.get();
+        } else {
+            throw new RuntimeException("Did not find the bird you are looking for - "+id);
+        }
+
+        return bird;
     }
 
     @Override
     public void save(Bird bird) {
+        birdsRepository.save(bird);
 
     }
 
     @Override
     public void deleteById(int id) {
-
+        birdsRepository.deleteById(id);
     }
 }
