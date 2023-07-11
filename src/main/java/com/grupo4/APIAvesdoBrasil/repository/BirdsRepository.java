9f21a9bc7609a378fe0b6acda06e4d99e9f1452c
beatrizfteixeira <biaft03@protonmail.com>
2023-07-11 16:04:50 -0300

    adding POST create endpoint

--- src/main/java/com/grupo4/APIAvesdoBrasil/repository/BirdsRepository.java
+++ src/main/java/com/grupo4/APIAvesdoBrasil/repository/BirdsRepository.java
@@ -2,6 +2,8 @@ package com.grupo4.APIAvesdoBrasil.repository;
 
 import com.grupo4.APIAvesdoBrasil.entity.Bird;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BirdsRepository extends JpaRepository<Bird, Integer> {
 }
--- src/main/java/com/grupo4/APIAvesdoBrasil/service/BirdService.java
+++ src/main/java/com/grupo4/APIAvesdoBrasil/service/BirdService.java
@@ -2,9 +2,13 @@ package com.grupo4.APIAvesdoBrasil.service;
 
 import com.grupo4.APIAvesdoBrasil.entity.Bird;
 import com.grupo4.APIAvesdoBrasil.repository.BirdsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BirdService {
 
+    @Autowired
     private BirdsRepository birdsRepository;
 
     public Bird addNewBird(Bird bird) {
