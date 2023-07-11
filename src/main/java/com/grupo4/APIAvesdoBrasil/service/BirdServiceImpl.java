846fe4163c7005da2fcfef0757733c2c0adcdbd4
Ricardo Matsumura <fuzilo@hotmail.com>
2023-07-10 23:16:56 -0300

    Adicionado services e implementacao dos metodos

--- src/main/java/com/grupo4/APIAvesdoBrasil/service/BirdServiceImpl.java
+++ src/main/java/com/grupo4/APIAvesdoBrasil/service/BirdServiceImpl.java
@@ -1,50 +1,28 @@
 package com.grupo4.APIAvesdoBrasil.service;
 
 import com.grupo4.APIAvesdoBrasil.entity.Bird;
-import com.grupo4.APIAvesdoBrasil.repository.BirdsRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
-@Service
 public class BirdServiceImpl implements BirdService{
 
-    private BirdsRepository birdsRepository;
-    @Autowired
-    public BirdServiceImpl(BirdsRepository theBirdsRepository){
-        birdsRepository = theBirdsRepository;
-    }
     @Override
     public List<Bird> findAll() {
-
-
-        return birdsRepository.findAll();
+        return null;
     }
 
     @Override
     public Bird findById(int id) {
-        Optional<Bird> result = birdsRepository.findById(id);
-        Bird bird = null;
-
-        if (result.isPresent()){
-            bird = result.get();
-        } else {
-            throw new RuntimeException("Did not find the bird you are looking for - "+id);
-        }
-
-        return bird;
+        return null;
     }
 
     @Override
     public void save(Bird bird) {
-        birdsRepository.save(bird);
 
     }
 
     @Override
     public void deleteById(int id) {
-        birdsRepository.deleteById(id);
+
     }
 }
