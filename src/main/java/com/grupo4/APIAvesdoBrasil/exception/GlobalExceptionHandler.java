3c4257591b8bf5be818a232b04efaa298a25c8a6
Ricardo Matsumura <ricardo.matsumura.pb@compasso.com.br>
2023-07-13 14:58:37 -0300

    Adicionado Exceptions

---
+++ src/main/java/com/grupo4/APIAvesdoBrasil/exception/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.grupo4.APIAvesdoBrasil.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // handle especific exceptions
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest){
+
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+//        ResourceNotFoundException resourceNotFoundException = new ResourceNotFoundException(super);
+
+        return  new ResponseEntity<>(errorDetails, HttpStatus.FOUND);
+    }
+    @ExceptionHandler(BirdAPIException.class)
+    public ResponseEntity<ErrorDetails> handleBidAPIException (BirdAPIException exception, WebRequest webRequest){
+            ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+//                ErrorDetails errorDetails = new ErrorDetails(""));
+            return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception, WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(),exception.getMessage(),webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
+
+
