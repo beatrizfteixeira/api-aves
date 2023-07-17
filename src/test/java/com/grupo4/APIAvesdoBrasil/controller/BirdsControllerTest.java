3a3395a2c2a39e0ebee2c36727c7c4751ec251a9
Marlon Vinicius <marlon.vinicius2@gmail.com>
2023-07-17 16:06:29 -0300

    Integrations Tests

--- src/test/java/com/grupo4/APIAvesdoBrasil/controller/BirdsControllerTest.java
+++ src/test/java/com/grupo4/APIAvesdoBrasil/controller/BirdsControllerTest.java
@@ -22,7 +22,10 @@ import java.util.Arrays;
 import java.util.List;
 
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 class BirdsControllerTest {
@@ -39,8 +42,6 @@ class BirdsControllerTest {
     void setUp() {
         MockitoAnnotations.initMocks(this);
         mockMvc = MockMvcBuilders.standaloneSetup(birdsController).build();
-
-
     }
 
 
@@ -51,18 +52,18 @@ class BirdsControllerTest {
         List<Bird> birdsList = Arrays.asList(bird1, bird2);
         when( birdService.findAll()).thenReturn(birdsList);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/bird")
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/birds")
                         .contentType(MediaType.APPLICATION_JSON))
-                        .andExpect(MockMvcResultMatchers.status().isOk())
-                        .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
-                        .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(1))
-                        .andExpect(MockMvcResultMatchers.jsonPath("$[0].commonName").value("pardal"))
-                        .andExpect(MockMvcResultMatchers.jsonPath("$[0].scientificName").value("Passer domesticus"))
-                        .andExpect(MockMvcResultMatchers.jsonPath("$[0].description").value("Common found Bird"))
-                        .andExpect(MockMvcResultMatchers.jsonPath("$[1].id").value(2))
-                        .andExpect(MockMvcResultMatchers.jsonPath("$[1].scientificName").value("Anas platyrhynchos"))
-                        .andExpect(MockMvcResultMatchers.jsonPath("$[1].description").value("Common livestock bird"));
-        Mockito.verify(birdService).findAll();
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].commonName").value("pardal"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].scientificName").value("Passer domesticus"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].description").value("Common found Bird"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].id").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].scientificName").value("Anas platyrhynchos"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].description").value("Common livestock bird"));
+        verify(birdService).findAll();
 
     }
     private String asJsonString(Object obj) {
@@ -89,9 +90,7 @@ class BirdsControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.scientificName").value("Passer domesticus"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.description").value("Common found Bird"));
 
-
-        Mockito.verify(birdService, Mockito.times(1)).save(bird1);
-
+        verify(birdService, times(1)).save(bird1);
     }
 
     @Test
@@ -102,29 +101,76 @@ class BirdsControllerTest {
 
         mockMvc.perform(MockMvcRequestBuilders.get("/api/bird/{id}", 2)
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNotFound());
-//                .andExpect(status().isOk())
-//                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(jsonPath("$.id").value(birdId))
-//                .andExpect(jsonPath("$.commonName").value("pardal"))
-//                .andExpect(jsonPath("$.scientificName").value("Passer domesticus"))
-//                .andExpect(jsonPath("$.description").value("Common found Bird"));
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(birdId))
+                .andExpect(jsonPath("$.commonName").value("pardal"))
+                .andExpect(jsonPath("$.scientificName").value("Passer domesticus"))
+                .andExpect(jsonPath("$.description").value("Common found Bird"));
+    }
 
+    @Test
+    void testGetBirdByName() throws Exception {
+        // Create a mock bird
+        Bird mockBird = new Bird();
+        mockBird.setId(1);
+        mockBird.setCommonName("pardal");
+        mockBird.setScientificName("Passer domesticus");
+        mockBird.setDescription("Common found Bird");
 
+        when(birdService.findByName("pardal")).thenReturn(mockBird);
+
+        mockMvc.perform(get("/api/bird/name/pardal"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.commonName").value("pardal"))
+                .andExpect(jsonPath("$.scientificName").value("Passer domesticus"))
+                .andExpect(jsonPath("$.description").value("Common found Bird"));
+
+        verify(birdService, times(1)).findByName("pardal");
     }
 
     @Test
     void testDeleteBirdById() throws Exception {
-        Bird bird1 = new Bird(1, "pardal", "Passer domesticus", "Common found Bird");
-        birdService.save(bird1);
-        when(birdService.deleteById(1)).thenReturn(bird1);
+        // Create a mock bird
+        Bird mockBird = new Bird();
+        mockBird.setId(1);
+        mockBird.setCommonName("pardal");
+        mockBird.setScientificName("Passer domesticus");
+        mockBird.setDescription("Common found Bird");
 
+        when(birdService.deleteById(1)).thenReturn(mockBird);
 
-        mockMvc.perform(MockMvcRequestBuilders.delete("/api/bird/{id}", 1)
-                        .contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(delete("/api/bird/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_PLAIN))
-                .andExpect(content().string("DELETED!"));
+                .andExpect(content().string("BIRD DELETED!"));
 
+        verify(birdService, times(1)).deleteById(1);
+    }
+
+    @Test
+    void testUpdateBirdById() throws Exception {
+        Bird mockBird = new Bird();
+        mockBird.setId(1);
+        mockBird.setCommonName("pardal");
+        mockBird.setScientificName("Passer domesticus");
+        mockBird.setDescription("Common found Bird");
+
+        when(birdService.findById(1)).thenReturn(mockBird);
+
+        String requestBody = "{ \"commonName\": \"Pato\", \"scientificName\": \"Anas platyrhynchos\", \"description\": \"Common livestock bird\" }";
+
+        mockMvc.perform(put("/api/bird/1/update")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.commonName").value("pardal"))
+                .andExpect(jsonPath("$.scientificName").value("Passer domesticus"))
+                .andExpect(jsonPath("$.description").value("Common found Bird"));
+
+        verify(birdService, times(1)).updateCommonName("Pato", 1);
+        verify(birdService, times(1)).updateScientificName("Anas platyrhynchos", 1);
+        verify(birdService, times(1)).updateDescription("Common livestock bird", 1);
     }
 }
